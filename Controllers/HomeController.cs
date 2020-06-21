feb7de28ef55756036bf5ae298a97ad762a2123b
kenjilam92 <kenji.lam.92@gmail.com>
2020-06-20 23:49:40 -0400

    completed on website

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using wedding_planner.Models;
 
 namespace wedding_planner.Controllers
@@ -23,7 +24,18 @@ namespace wedding_planner.Controllers
         [Route("")]
         public IActionResult Index()
         {   
-            return View();
+            if(HttpContext.Session.GetInt32("userId") == null)
+            {
+                return View();
+            }
+            return View("AddWedding");
+        }
+        
+        [Route("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("/");
         }
         [HttpPost]
         [Route("register")]
@@ -34,13 +46,14 @@ namespace wedding_planner.Controllers
                 if(databases.Users.Any(u => u.Email == newuser.Email))
                 {
                     ModelState.AddModelError("Email","This email is already registed. Please Login!");
+                    return View("Index");
                 }
                 string hash = regHasher.HashPassword(newuser,newuser.Password);
                 newuser.Password = hash;
                 databases.Users.Add(newuser);
                 databases.SaveChanges();
                 HttpContext.Session.SetInt32("userId", newuser.UserId);
-                return Redirect("/");
+                return Redirect("/dashboard");
             }
             return View("Index");
         }
@@ -62,11 +75,91 @@ namespace wedding_planner.Controllers
                     return View("Index");
                 }
                 HttpContext.Session.SetInt32 ("userId", userInDB.UserId);
-                return Redirect("/");
+                return Redirect("/dashboard");
             }
             return View("Index");
         }
+        ////////////////////////////// Dashboard
+        [HttpGet]
+        [Route("dashboard")]
+
+        public IActionResult Dashboard()
+        {   
+            if(HttpContext.Session.GetInt32("userId") == null)
+            {
+                return Redirect("/");
+            }
+            ViewBag.UserId = HttpContext.Session.GetInt32("userId");
+            ViewBag.WeddingPlans = databases.Weddings
+                .Include(w => w.AllGuests)
+                .ThenInclude( a => a.User)
+                .OrderBy(w => w.Date)
+                .ToList();
+            return View();
+        }
+        [Route("wedding/{id}")]
+        public IActionResult WeddingDetails(int id)
+        {
+            if(HttpContext.Session.GetInt32("userId") == null)
+            {
+                return Redirect("/");
+            }
+            ViewBag.Wedding = databases.Weddings
+                .Include(w => w.AllGuests)
+                .ThenInclude(a => a.User)
+                .FirstOrDefault(w => w.WeddingId == id);
+            return View("WeddingDetails");
+        }
+        [Route("wedding/{id}/{cmd}")]
+        public IActionResult WeddingJoin(int id, string cmd)
+        {   
+            if(HttpContext.Session.GetInt32("userId") == null)
+            {
+                return Redirect("/");
+            }
+            Wedding selected = databases.Weddings.FirstOrDefault(w => w.WeddingId == id);
+            int? userId = HttpContext.Session.GetInt32("userId");
+            if (cmd =="cancel")
+            {
+                databases.Weddings.Remove(selected);
+                databases.SaveChanges();
+            }
+            else if(cmd == "RSVP")
+            {
+                Associate join = new Associate();
+                join.UserId = (int) userId;
+                join.WeddingId = id;
+                databases.Associates.Add(join);
+                databases.SaveChanges();
+            }
+            else if (cmd == "UnRSVP")
+            {
+                Associate remove = databases.Associates
+                    .FirstOrDefault(a => a.UserId == userId && a.WeddingId == id);
+                databases.Associates.Remove(remove);
+                databases.SaveChanges();
+
+            }
+            return Redirect("/dashboard");
+
+        }
 
 
+        [HttpPost("wedding/create")]
+        public IActionResult CreateWedding(Wedding Plan)
+        {   
+            if(HttpContext.Session.GetInt32("userId") == null)
+            {
+                return Redirect("/");
+            }
+            if(ModelState.IsValid)
+            {    
+                Plan.UserId = (int) HttpContext.Session.GetInt32("userId");
+                databases.Weddings.Add(Plan);
+                databases.SaveChanges();
+                return Redirect("/dashboard");
+            }   
+            return View("AddWedding");
+        }
     }
 }
